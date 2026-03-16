8244e018cbe727282d683056c93aa2373999f724
Fatima <fatii25785@gmail.com>
2026-03-17 01:54:08 +0500

    Add files via upload
    string operation

---
+++ string operations.py
@@ -0,0 +1,20 @@
+Name="My name is fatima ali"
+print(Name.upper())
+print(Name.lower())
+print(Name.capitalize())
+print(Name.title())
+print(Name.swapcase())
+
+st="Myageis20"
+print(st[::])
+print(st[1::1])
+print(st[2:8:])
+print(st[-1::])
+print(st[3:7:2])
+
+st="hyy!My name is Fatima"
+n=len(st)
+print(n)
+
+for i in "Finally":
+ print(i)
